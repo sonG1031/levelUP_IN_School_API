69e3a3228cd6578c6300f91d420096c52f4a9c89
송민섭 <minsub1031@gmail.com>
2022-11-03 21:25:03 +0900

    Update quest.py

--- api/route/quest.py
+++ api/route/quest.py
@@ -25,7 +25,7 @@ def app_quest(teacher_id): # 자신이 생성한 퀘스트 보기(GET), 퀘스�
             class_code = request.json['class_code']
             # teacher_id = request.json['teacher_id']
 
-            user_lst = User.query.filter(and_(User.class_code == class_code, User.job == '�л�'))
+            user_lst = User.query.filter(and_(User.class_code == class_code, User.job == '학생'))
             print(list(user_lst))
             if not user_lst:
                 return jsonify({
