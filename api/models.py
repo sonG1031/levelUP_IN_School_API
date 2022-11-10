d12f6239f0d671fa33b2ab0b01e8aa650f026caf
송민섭 <minsub1031@gmail.com>
2022-11-10 20:54:42 +0900

    Update models.py

--- api/models.py
+++ api/models.py
@@ -54,8 +54,8 @@ class UserQuest(db.Model):
     user_id = db.Column(db.String(200), db.ForeignKey('user.user_id', ondelete='CASCADE'), nullable=False)
     teacher_id = db.Column(db.String(200), nullable=False) # 자기가 만든 퀘스트가 뭔지 알기 위해서
     user = db.relationship('User', backref=db.backref('Quest_set', cascade='all, delete-orphan'))
-    done = db.Column(db.Boolean, server_default='False', nullable=True)
-    check = db.Column(db.Boolean, server_default='False', nullable=True)
+    done = db.Column(db.Boolean, server_default='', nullable=True)
+    check = db.Column(db.Boolean, server_default='', nullable=True)
     questlst_id = db.Column(db.Integer, db.ForeignKey('quest_list.id', ondelete='CASCADE'), nullable=False)
     questlst = db.relationship('QuestList', backref=db.backref('QuestList_set', cascade='all, delete-orphan'))
 
