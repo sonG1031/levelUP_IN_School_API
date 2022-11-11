fef59ee17bfde51acbad24df75dba49942c6e29b
송민섭 <minsub1031@gmail.com>
2022-11-11 21:21:40 +0900

    Update quest.py

--- api/route/quest.py
+++ api/route/quest.py
@@ -215,7 +215,7 @@ def game_quest(user_id): # 자신의 퀘스트 목록 가져오기(GET), 퀘스�
                 "data": user_quest
             })
         elif request.method == "POST": # 퀘스트 완료 요청
-            user_quest = UserQuest.query.filter_by(user_id=user_id).filter_by(id=request.json['id']).first()
+            user_quest = UserQuest.query.filter(and_(UserQuest.user_id == user_id, UserQuest.id == request.json['id']))
             now = datetime.datetime.now()
 
             if user_quest.start_date > now:
